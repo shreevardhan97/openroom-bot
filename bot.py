bb0a37c61afa05d9c7f34bb3f71fa79914876898
Shreevardhan Shah <shreevardhanshah@gmail.com>
2021-02-26 19:31:07 +0530

    adding all files

---
+++ bot.py
@@ -0,0 +1,66 @@
+import discord
+from discord.ext import commands
+import requests
+from requests.api import request
+import random
+import os
+
+# Client (our bot)
+client = commands.Bot(command_prefix='!openroom ')
+
+# gifs
+gify = ["https://gph.is/g/aQQDGMV",
+        "https://gph.is/g/E0WYq8o",
+        "http://gph.is/2uu95WB",
+        "http://gph.is/2aguawW",
+        "https://gph.is/g/4bjzgmo",
+        "http://gph.is/1CBEAKL"]
+l = len(gify)
+helptxt = " \"!openroom startjam <topic_without_space>\" "
+support = "Reach out admins/openhouse reps"
+
+
+@client.command(name='startjam')
+async def startjam(context, *, message):
+    i = random.randint(0, 5)
+    await context.message.channel.send(gify[i])
+    print("test")
+    print(context.message.content)
+    str1 = context.message.content
+    temp = str1.split()
+    str2 = temp[2]
+    author_id = context.message.author
+    payload = {
+        "type": "group_study"
+    }
+    url = "https://dev.meets.api.openhouse.study/create/"
+    request = requests.post(
+        url, headers={"Content-Type": 'application/json'}, params={}, json=payload)
+    linkopenroom = request.json()["room_url"]
+    print(linkopenroom)
+    myEmbed = discord.Embed(title="OpenRoom", color=0x00ff00)
+    myEmbed.add_field(name="Session by", value=author_id)
+    myEmbed.add_field(name="Topic", value=str2, inline=False)
+    myEmbed.add_field(name="Joining Link", value=linkopenroom, inline=False)
+    myEmbed.set_footer(text='powered by openhouse')
+    await context.message.channel.send(embed=myEmbed)
+    str2 = ""
+
+
+@client.command(name='faq')
+async def faq(context):
+    myEmbed = discord.Embed(title="OpenRoom Faq", color=0x00ff00)
+    myEmbed.add_field(name="How to start a session?", value=helptxt)
+    myEmbed.add_field(name="Any other questions?", value=support, inline=False)
+    myEmbed.set_footer(text='powered by openhouse')
+    await context.channel.send(embed=myEmbed)
+# Checking if the bot is online
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('clubbing at Openhouse'))
+    print('Pikachu!')
+
+# Run the bot on server
+client.run(os.environ['DISCORD_TOKEN'])
